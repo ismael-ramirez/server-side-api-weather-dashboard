6cd1283e64e837771a0eb1a148d7d2256aa7eff9
Ismael Ramirez <ismael-ramirez@live.com>
2020-08-21 14:32:14 -0700

    server-side-api-weather-dashboard

---
+++ README.md
@@ -0,0 +1,30 @@
+
+This is a weather dashboard application for travelers. It 
+provides a weather outlook for multiple cities with form 
+inputs for planning trips accordingly. When you search for 
+a city, you are presented with current and future conditions 
+for that city, then that city is added to the search history.
+When viewing current weather conditions for that city, you 
+are then presented with the city name, the date, an icon 
+representation of weather conditions, the temperature, 
+the humidity, the wind speed, and the UV index.When viewing 
+the UV index you are presented with a color that indicates 
+whether the conditions are favorable, moderate, or severe. 
+When viewing future weather conditions for that city then 
+you are presented with a 5-day forecast that displays 
+the date, an icon representation of weather conditions, the 
+temperature, and the humidity. When the icon of the city 
+gets clicked in the search history then you are again 
+presented with current and future conditions for that city. 
+This application uses the OpenWeather API to retrieve weather
+data and also localStorage to store persistent data. 
+Application user interface is clean and easy to navigate. 
+
+Repository contains quality README file with description, screenshot, and link to deployed application
+
+How to Submit the Challenge
+You are required to submit BOTH of the following for review:
+
+The URL of the functional, deployed application.
+
+The URL of the GitHub repository. Give the repository a unique name and include a README describing the project
---
+++ script.js
@@ -0,0 +1,165 @@
+$(document).ready(function(){
+    var cityArray=[];
+    var APIKey = "258635a5e9f4d564a966e4ce880b065d";
+    $("#weatherBox").hide();
+
+  
+  function getHistory(){
+       var history = localStorage.getItem("cityList");
+       if(history){
+           cityArray=JSON.parse(history);
+           for(var i=0;i<cityArray.length;i++){
+               addCity(cityArray[i]);
+           }
+           searchWeather(cityArray[cityArray.length-1]);
+       }
+   
+   }
+   getHistory();
+
+   function addCity(city){
+        var ulElement=$("ul");
+        var liElement=$("<li>")
+        var buttonElement=$("<button>");
+        buttonElement.attr("class","cityListBtn");
+        buttonElement.text(city);
+        liElement.append(buttonElement)
+        ulElement.append(liElement);
+    }
+
+    $(document).on("click",".cityListBtn",function(){
+        event.preventDefault();
+        var cityClick = $(this).text();
+        console.log(cityClick);
+        searchWeather(cityClick);
+    })
+
+
+    function searchWeather (city){
+        var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
+            var uvURL="";
+            
+            $.ajax({
+                url:weatherURL,
+                method:"GET"
+            }).then(function(response){
+                console.log(response);
+    
+                var icon = response.weather[0].icon;
+                var iconURL = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
+                $("#cityName").text(response.name);
+                var imgDiv=$("<img>");
+                imgDiv.attr("src",iconURL);
+                var currentDate = moment().format("MM/DD/YYYY");
+                var spanDate=$("<span>").text(" (" + currentDate +")");
+                $("h4").append(spanDate);
+                $("h4").append(imgDiv);
+    
+                var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+                tempF=tempF.toFixed(2);
+                $("#temperature").text("Temperature: " + tempF + " °F");
+    
+                
+                $("#humidity").text("Humidity: " + response.main.humidity + " %");
+                $("#windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
+    
+                
+                var lon=response.coord.lon;
+                var lat=response.coord.lat;
+                uvURL="https://api.openweathermap.org/data/2.5/uvi?appid="+APIKey+"&lat="+lat+"&lon="+
+                lon;
+        
+                    $.ajax({
+                        url:uvURL,
+                        method:"GET"
+                    }).then(function(uvData){
+                        console.log(uvData);
+
+                        var uvNumber = uvData.value;
+                        $("#UV").text("UV Index: ");
+                        var spanEle = $("<span>").text(uvNumber);
+                        $("#UV").append(spanEle);
+                        
+                    
+                        if(uvNumber<3){
+                            spanEle.removeClass("low moderate high veryHigh extreme").addClass("low");
+                            
+                        }else if(uvNumber>=3 && uvNumber<6){
+                            spanEle.removeClass("low moderate high veryHigh extreme").addClass("moderate");
+                     
+                        }else if(uvNumber>=6 && uvNumber<8){
+                            spanEle.removeClass("low moderate high veryHigh extreme").addClass("high");
+                          
+                        }else if(uvNumber>=8 && uvNumber<11){
+                            spanEle.removeClass("low moderate high veryHigh extreme").addClass("veryHigh");
+                           
+                        }else{
+                            spanEle.removeClass("low moderate high veryHigh extreme").addClass("extreme");
+                           
+                        }
+    
+                        $("#weatherBox").show();  
+                    })
+    
+                
+                 var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+APIKey;
+                    
+                 $("#forecast").text("5-Day Forecast: ");
+    
+                    $.ajax({
+                        url:forecastURL,
+                        method:"GET"
+                    }).then(function(forecastData){
+                        console.log(forecastData);
+    
+                        $("#forecastBox").empty();
+    
+                        for(var i=0; i<forecastData.list.length; i++){
+                            if (forecastData.list[i].dt_txt.indexOf("15:00:00")!== -1){
+    
+                                var newCardDiv = $("<div>").addClass("card forecastCard");
+                                
+                                var currentDate = moment(forecastData.list[i].dt_txt).format("MM/DD/YYYY");
+                                var forecastDate = $("<h5>").text(currentDate);
+    
+                                var newIcon = forecastData.list[i].weather[0].icon;
+                                var newIconURL = "https://openweathermap.org/img/wn/"+newIcon+"@2x.png";
+                                var imgIcon = $("<img>").attr("src",newIconURL);
+                      
+                                var tempFF = (forecastData.list[i].main.temp - 273.15) * 1.80 + 32;
+                                tempFF=tempFF.toFixed(2);
+                                var tempElement = $("<p>").text("Temp: " + tempFF +" °F");
+    
+                                var humElement = $("<p>").text("Humidity: " + forecastData.list[i].main.humidity +"%");
+    
+                                newCardDiv.append(forecastDate, imgIcon,tempElement,humElement);
+    
+                                $("#forecastBox").append(newCardDiv);
+      
+                            }
+    
+                        }
+    
+                    })
+            })
+    }
+
+    
+    $("#searchBtn").on("click",function(){
+        event.preventDefault();
+        var city = $("#userInput").val();
+        $("#userInput").val("");
+        if(city===""){
+            return
+        };
+  
+        if(cityArray.indexOf(city)===-1){
+            addCity(city);
+            cityArray.push(city);
+            localStorage.setItem("cityList",JSON.stringify(cityArray));
+          
+        };
+        searchWeather(city);
+    })
+
+})
